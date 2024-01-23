17ae5216a7f1469b618e15116259820e37883f24
meol <dotnet.caboodle@gmail.com>
2024-01-23 23:17:27 +0000

    contact details placeholder

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,53 +1,72 @@
 import { useState } from "react";
 
 function Contact() {
-  const [formData, setFormData] = useState({name: "",email: "",message: ""});
-  
-  const handleChange = (event:any) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event: any) => {
     const { name, value } = event.target;
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
   };
 
-  const handleSubmit = (event:any) => {
+  const handleSubmit = (event: any) => {
     event.preventDefault();
-    alert(`Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}` );
-  }
+    alert(
+      `Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}`
+    );
+  };
 
   return (
     <section id="contact" className="text-center">
       <div className="container">
         <h1>CONTACT US</h1>
-        <p className="mb-5">
-          We are here to help our clients succeed in their digital transformation endeavors.
-          <br/>Get in touch with us today. We would love to hear from you.<br />
+        <p className="mt-3">
+          We are here to help our clients succeed in their digital
+          transformation endeavors.
+          <br />
+          Get in touch with us today. We would love to hear from you.
+          <br />
         </p>
-
-        <div className="container">
-          TEL: +353 1 123 3388
-          EMAIL: robusoft.limited@gmail.com
-          <p className="mb-5">Letterkenny House, Donegal,
-          Republic of Ireland</p>
-        </div>
-        
-        <form onSubmit={handleSubmit} className="contact-form col-md-11 col-lg-9 mx-auto">
+        <span><i className="ti-mobile"></i>TEL: +353 1 123 3388</span><br/>
+        <span>EMAIL: robusoft.limited@gmail.com</span>
+        <p className="mb-5">Letterkenny House, Donegal, Republic of Ireland</p>
+        <form
+          onSubmit={handleSubmit}
+          className="contact-form col-md-11 col-lg-9 mx-auto"
+        >
           <div className="form-row">
             <div className="col form-group">
-              <input type="text" id="name" name="name"
-                value={formData.name} onChange={handleChange}
-                className="form-control" placeholder="Name" />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="form-control"
+                placeholder="Name"
+              />
             </div>
             <div className="col form-group">
-              <input id="email" name="email"
-                type="email" value={formData.email} onChange={handleChange}
+              <input
+                id="email"
+                name="email"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
                 className="form-control"
                 placeholder="Email"
               />
             </div>
           </div>
           <div className="form-group">
-            <textarea  
-              name="message" id="message"
-              value={formData.message} onChange={handleChange}
+            <textarea
+              name="message"
+              id="message"
+              value={formData.message}
+              onChange={handleChange}
               className="form-control"
               placeholder="Your Message"
             ></textarea>
@@ -63,7 +82,6 @@ function Contact() {
       </div>
       <p>&copy;2024</p>
     </section>
-    
   );
 }
 
