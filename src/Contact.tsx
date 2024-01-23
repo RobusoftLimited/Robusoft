5b80a5c996934876aa9dafb1271cd51cc7d40519
meol <dotnet.caboodle@gmail.com>
2024-01-23 22:31:58 +0000

    read form values in preparation for the email feature

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
 function Contact() {
+  const [formData, setFormData] = useState({name: "",email: "",message: ""});
+  
+  const handleChange = (event:any) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event:any) => {
+    event.preventDefault();
+    alert(`Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}` );
+  }
+
   return (
     <section id="contact" className="text-center">
       <div className="container">
@@ -8,23 +22,25 @@ function Contact() {
           <br/>If you have some questions or need help, Please Contact Us <br />
         </p>
 
-        <form className="contact-form col-md-11 col-lg-9 mx-auto">
+        <form onSubmit={handleSubmit} className="contact-form col-md-11 col-lg-9 mx-auto">
           <div className="form-row">
             <div className="col form-group">
-              <input type="text" className="form-control" placeholder="Name" />
+              <input type="text" id="name" name="name"
+                value={formData.name} onChange={handleChange}
+                className="form-control" placeholder="Name" />
             </div>
             <div className="col form-group">
-              <input
-                type="email"
+              <input id="email" name="email"
+                type="email" value={formData.email} onChange={handleChange}
                 className="form-control"
                 placeholder="Email"
               />
             </div>
           </div>
           <div className="form-group">
-            <textarea
-              name=""
-              id=""
+            <textarea  
+              name="message" id="message"
+              value={formData.message} onChange={handleChange}
               className="form-control"
               placeholder="Your Message"
             ></textarea>
