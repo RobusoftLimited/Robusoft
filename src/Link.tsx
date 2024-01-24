1259243a403b63c41ee32af1e27dad2b89d08874
meol <dotnet.caboodle@gmail.com>
2024-01-24 21:59:12 +0000

    add cv download

--- src/Link.tsx
+++ src/Link.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 import linkedin from "./images/linkedin.jpeg";
 
 function Link() {
+
+  const cv_url='https://docs.google.com/document/d/1tw3jcIqGVt2jOzkyPVOBaw15eL7rJPGV/edit?usp=sharing&ouid=104973433710641980147&rtpof=true&sd=true';
+
   return (
     <section id="link" className="text-center">
       <div className="container">
@@ -16,9 +19,9 @@ function Link() {
             src={linkedin}
             style={{ width: '300px', height: '150px' }}
             alt="linkedin icon"
-            className="mb-3"
           />
         </a>
+        <h4><i className="ti-download"></i><a href={cv_url} download="cv.docx">CV - Geronimo Alcantara</a></h4>
       </div>
       <p className="mt-5">&copy;2024</p>
     </section>
