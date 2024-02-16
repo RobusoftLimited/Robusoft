6214ce1558275ce609724bf88b9653974c404f3b
meol <dotnet.caboodle@gmail.com>
2024-02-16 22:43:06 +0000

    fix contact, and reformat for readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   let toggleMe = false;
   const [isActive, setIsActive] = useState<boolean>(toggleMe);
-  
+
   function hideNavbar(event: any): void {
-    setIsActive(toggleMe => !toggleMe);
-    console.log('clicky');
+    setIsActive((toggleMe) => !toggleMe);
+    console.log("clicky");
   }
 
   return (
@@ -21,7 +21,10 @@ function App() {
         data-offset-top="20"
       >
         <div className="container">
-          <a className="navbar-brand" href="https://robusoftlimited.github.io/Robusoft/">
+          <a
+            className="navbar-brand"
+            href="https://robusoftlimited.github.io/Robusoft/"
+          >
             <strong className="text-primary">ROBUSOFT</strong>{" "}
           </a>
           <button
@@ -37,52 +40,81 @@ function App() {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div onClick={hideNavbar} className={isActive ? "collapse navbar-collapse show": "collapse navbar-collapse"} 
-                id="navbarSupportedContent">
+          <div
+            onClick={hideNavbar}
+            className={
+              isActive
+                ? "collapse navbar-collapse show"
+                : "collapse navbar-collapse"
+            }
+            id="navbarSupportedContent"
+          >
             <ul className="navbar-nav ml-auto">
-              <li className="nav-item" data-toggle="collapse"
-            data-target="#navbarSupportedContent" >
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+              >
                 <a className="nav-link" href="#home">
                   Home
                 </a>
               </li>
-              <li className="nav-item" data-toggle="collapse"
-            data-target="#navbarSupportedContent">
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+              >
                 <a className="nav-link" href="#about">
                   About
                 </a>
               </li>
-              <li className="nav-item" data-toggle="collapse"
-            data-target="#navbarSupportedContent">
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+              >
                 <a className="nav-link" href="#content">
                   Mission
                 </a>
               </li>
-              <li className="nav-item" data-toggle="collapse"
-            data-target="#navbarSupportedContent">
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+              >
                 <a className="nav-link" href="#services">
                   Services
                 </a>
               </li>
-              <li className="nav-item" data-toggle="collapse"
-            data-target="#navbarSupportedContent">
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+              >
                 <a className="nav-link" href="#feature">
                   Portfolio
                 </a>
               </li>
-              <li className="nav-item">
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+              >
                 <a className="nav-link" href="#contact">
                   Contact
                 </a>
               </li>
-              <li className="nav-item" data-toggle="collapse"
-            data-target="#navbarSupportedContent">
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+              >
                 <a className="nav-link" href="#link">
                   Links
                 </a>
               </li>
             </ul>
-            <ToastContainer/>
+            <ToastContainer />
           </div>
         </div>
       </nav>
